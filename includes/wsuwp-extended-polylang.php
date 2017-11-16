8b636a9b79ab4a18404a91697e7379a5e6f204c4
Phil Cable <pcable@wsu.edu>
2017-11-16 10:02:59 -0800

    Remove settings that require the pro version of Polylang

--- includes/wsuwp-extended-polylang.php
+++ includes/wsuwp-extended-polylang.php
@@ -1,3 +1,31 @@
 <?php
 
 namespace WSUWP\Polylang\Extended;
+
+add_filter( 'pll_settings_modules', 'WSUWP\Polylang\Extended\filter_settings' );
+
+/**
+ * Removes settings that require the pro version
+ * (or could otherwise get users into trouble).
+ *
+ * @since 0.0.1
+ *
+ * @param array
+ *
+ * @return array
+ */
+function filter_settings( $modules ) {
+	$unset_modules = array(
+		'PLL_Settings_Media',
+		'PLL_Settings_Sync',
+		'PLL_Settings_WPML',
+		'PLL_Settings_Share_Slug',
+		'PLL_Settings_Translate_Slugs',
+		'PLL_Settings_Tools',
+		'PLL_Settings_Licenses',
+	);
+
+	$modules = array_diff( $modules, $unset_modules );
+
+	return $modules;
+}
