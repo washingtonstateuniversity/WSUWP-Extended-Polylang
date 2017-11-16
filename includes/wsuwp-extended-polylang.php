4c804cb6a5ee16fcc6ba2c27753429bd9c71e8d4
Phil Cable <pcable@wsu.edu>
2017-11-16 10:18:29 -0800

    Hide flag-related things from the "Languages" dashboard page

--- includes/wsuwp-extended-polylang.php
+++ includes/wsuwp-extended-polylang.php
@@ -5,6 +5,7 @@ namespace WSUWP\Polylang\Extended;
 add_filter( 'pll_settings_modules', 'WSUWP\Polylang\Extended\filter_settings' );
 add_filter( 'pll_settings_tabs', 'WSUWP\Polylang\Extended\filter_menu', 11 );
 add_filter( 'pll_predefined_flags', 'WSUWP\Polylang\Extended\filter_flags' );
+add_action( 'admin_enqueue_scripts', 'WSUWP\Polylang\Extended\admin_enqueue_scripts' );
 
 /**
  * Removes settings that require the pro version
@@ -50,6 +51,10 @@ function filter_menu( $tabs ) {
 /**
  * Returns an empty array for flag values.
  *
+ * Although the <select> input for choosing a flag is hidden with CSS,
+ * this completely removes the options, which disables the default
+ * Polylang behavior of selecting a flag when a language is selected.
+ *
  * @since 0.0.1
  *
  * @param array
@@ -58,4 +63,18 @@ function filter_menu( $tabs ) {
  */
 function filter_flags( $flags ) {
 	return array();
+}
+
+/**
+ * Enqueues a stylesheet for hiding the flag setting and list table column
+ * on the "Languages" dashboard page.
+ *
+ * @since 0.0.1
+ *
+ * @param string $hook_suffix The current admin page.
+ */
+function admin_enqueue_scripts( $hook_suffix ) {
+	if ( 'toplevel_page_mlang' === $hook_suffix ) {
+		wp_enqueue_style( 'wsuwp-polylang-extended', plugins_url( 'css/admin-languages.css', dirname( __FILE__ ) ) );
+	}
 }
